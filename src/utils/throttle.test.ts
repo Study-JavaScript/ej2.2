7342e54119270fa7beadc4a9ba1849760b0eef52
Study-JavaScript <sektersytout@gmail.com>
2024-07-15 10:46:16 +0200

    refactor(app): Clean Code-> delete comments

--- src/utils/throttle.test.ts
+++ src/utils/throttle.test.ts
@@ -1,14 +1,9 @@
 import { throttle } from './throttle';
 
-// Mock function to use in tests
 const mockFunc = jest.fn();
 
-// // Define a time function to mock setTimeout
-// const mockSetTimeout = (callback: () => void, delay: number) => {
-//   callback();
-// };
 
-jest.useFakeTimers(); // Use Jest's fake timers to control setTimeout
+jest.useFakeTimers();
 
 describe('throttle function', () => {
   afterEach(() => {
@@ -18,49 +13,38 @@ describe('throttle function', () => {
   it('should call the function once', () => {
     const throttledFunc = throttle(mockFunc, 1000);
 
-    // Call the throttled function immediately
     throttledFunc();
 
-    // Fast-forward time by 1000ms
     jest.advanceTimersByTime(1000);
 
-    // Expect the mock function to have been called exactly once
     expect(mockFunc).toHaveBeenCalledTimes(1);
   });
 
   it('should not call the function multiple times within delay', () => {
     const throttledFunc = throttle(mockFunc, 1000);
 
-    // Call the throttled function multiple times
     throttledFunc();
     throttledFunc();
     throttledFunc();
 
-    // Fast-forward time by 1000ms
     jest.advanceTimersByTime(1000);
 
-    // Expect the mock function to have been called only once
     expect(mockFunc).toHaveBeenCalledTimes(1);
   });
 
   it('should not call the function multiple times within delay', () => {
     const throttledFunc = throttle(mockFunc, 1000);
   
-    // Call the throttled function multiple times
     throttledFunc();
     throttledFunc();
     throttledFunc();
   
-    // Fast-forward time by 500ms
     jest.advanceTimersByTime(500);
   
-    // Expect the mock function to have been called only once
     expect(mockFunc).toHaveBeenCalledTimes(1);
   
-    // Fast-forward time by another 500ms
     jest.advanceTimersByTime(500);
   
-    // Expect the mock function to have been called only once
     expect(mockFunc).toHaveBeenCalledTimes(1);
   });
   
@@ -69,20 +53,16 @@ describe('throttle function', () => {
   it('should call the function twice if delay has passed', () => {
     const throttledFunc = throttle(mockFunc, 1000);
 
-    // Call the throttled function twice with delay between calls
     throttledFunc();
     
-    // Fast-forward time by 500ms
     jest.advanceTimersByTime(500);
 
     throttledFunc();
 
-    // Fast-forward time by another 500ms
     jest.advanceTimersByTime(500);
 
     throttledFunc();
 
-    // Expect the mock function to have been called twice
     expect(mockFunc).toHaveBeenCalledTimes(2);
   });
 });
